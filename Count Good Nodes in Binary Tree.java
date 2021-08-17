92e3520f8440aafb36d8d278c70970b5b28b3634
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-08-17 22:59:47 +0530

    Add files via upload

---
+++ Count Good Nodes in Binary Tree.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    int count = 0;
+    public int goodNodes(TreeNode root) 
+    {
+        util(root , Integer.MIN_VALUE);
+        return count;
+    }
+    void util(TreeNode root , int max)
+    {
+        if(root==null)
+            return;
+        
+        if(root.val>=max)
+            count++;
+        
+        util(root.left , Math.max(max , root.val));
+        util(root.right , Math.max(max , root.val));
+    }
+}
